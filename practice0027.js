bbdeac2d102076338bdacd2a662f472841e23241
CodeGuy02 <86216164+CodeGuy02@users.noreply.github.com>
2022-12-22 21:11:12 -0500

    Create practice0027.js

---
+++ practice0027.js
@@ -0,0 +1,30 @@
+playField = document.getElementById("playField");
+controlField = document.getElementById("controlField");
+
+w = playField.clientWidth;
+h = playField.clientHeight;
+
+
+playFieldCanvas = document.getElementById("playFieldCanvas");
+k = kaboom({width:w, height:h, canvas:playFieldCanvas, global:true, debug:true});
+debug.inspect = true;
+
+scene("game", () => {
+
+	add([
+        "text",
+		text("Press space to view score", { width: width() }),
+	]);
+
+	keyPress("space", () => {
+		// passing custom data to another scene
+		go("score", ~~rand(100));
+	});
+
+    console.log("in game scene");
+});
+
+
+go("game");
+
+console.log('scene data= ' + JSON.stringify(k));
